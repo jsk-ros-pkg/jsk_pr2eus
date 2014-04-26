0afd410a617dccfd74785ed1aee1902d393681e2
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-04-27 00:31:40 +0900

    fix find_package components for groovy, generae missing package via generete-all-msg-srv.sh

--- pr2eus/catkin.cmake
+++ pr2eus/catkin.cmake
@@ -1,5 +1,21 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(pr2eus)
+set(pr2eus_catkin_components roseus rostest euscollada control_msgs nav_msgs dynamic_reconfigure )
+if($ENV{ROS_DISTRO} STREQUAL "groovy")
+  set(_ROS_PACKAGE_PATH $ENV{ROS_PACKAGE_PATH})
+  set(ENV{ROS_PACKAGE_PATH} ${CMAKE_SOURCE_DIR}:${_ROS_PACKAGE_PATH})
+  execute_process(COMMAND rosrun roseus generate-all-msg-srv.sh move_base_msgs)
+  execute_process(COMMAND rosrun roseus generate-all-msg-srv.sh pr2_msgs)
+  execute_process(COMMAND rosrun roseus generate-all-msg-srv.sh pr2_controllers_msgs)
+  execute_process(COMMAND rosrun roseus generate-all-msg-srv.sh sound_play)
+  set(ENV{ROS_PACKAGE_PATH} ${_ROS_PACKAGE_PATH})
+else()
+  set(pr2eus_catkin_components ${pr2eus_catkin_components} move_base_msgs pr2_msgs pr2_controllers_msgs sound_play)
+endif()
+find_package(catkin REQUIRED COMPONENTS ${pr2eus_catkin_components}
+  geneus # this load roseus.cmake, so it needs to be located in the end
+  )
+
 
 catkin_package(
     DEPENDS 
