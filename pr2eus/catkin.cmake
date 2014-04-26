e0da73f25df113f85ecec8188bfe939c3a97412e
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-04-27 02:07:17 +0900

    fix find_package components for groovy

--- pr2eus/catkin.cmake
+++ pr2eus/catkin.cmake
@@ -1,8 +1,10 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(pr2eus)
-find_package(catkin REQUIRED COMPONENTS roseus rostest euscollada
-  pr2_msgs pr2_controllers_msgs control_msgs move_base_msgs
-  nav_msgs dynamic_reconfigure sound_play
+set(pr2eus_catkin_components roseus rostest euscollada control_msgs nav_msgs dynamic_reconfigure )
+if(NOT ($ENV{ROS_DISTRO} STREQUAL "groovy"))
+  set(pr2eus_catkin_components ${pr2eus_catkin_components} move_base_msgs pr2_msgs pr2_controllers_msgs sound_play)
+endif()
+find_package(catkin REQUIRED COMPONENTS ${pr2eus_catkin_components}
   geneus # this load roseus.cmake, so it needs to be located in the end
   )
 
