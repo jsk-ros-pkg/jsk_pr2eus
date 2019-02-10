ba3cdd423eaa70f688c39603b477ef8031fe1047
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2019-02-10 01:13:23 +0000

    fix speak-test-action.py, more test on SAY, PLAY_FILE and buiding files

--- pr2eus/test/speak-test-action.py
+++ pr2eus/test/speak-test-action.py
@@ -15,17 +15,17 @@ NAME = 'test_speack_action'
 class TestSpeakAction(unittest.TestCase):
     _result = SoundRequestResult()
 
-    speak_msg = None
-    speak_msg_jp = None
+    speak_msg = []
+    speak_msg_jp = []
 
     def cb(self, msg):
         rospy.logwarn("received speak_msg")
-        self.speak_msg = msg.sound_request
+        self.speak_msg.append(msg.sound_request)
         self._as_en.set_succeeded(self._result)
 
     def cb_jp(self, msg):
         rospy.logwarn("received speak_msg_jp")        
-        self.speak_msg_jp = msg.sound_request
+        self.speak_msg_jp.append(msg.sound_request)
         self._as_jp.set_succeeded(self._result)
 
     def setUp(self):
@@ -37,25 +37,28 @@ class TestSpeakAction(unittest.TestCase):
 
     def test_speak(self):
         rospy.sleep(10)
+        rospy.logwarn("test speak")
         rospy.logwarn(self.speak_msg)
         rospy.logwarn(self.speak_msg_jp)
-        while self.speak_msg == None:
+        while self.speak_msg == []:
             rospy.logwarn("waiting for speak_msg")
             rospy.sleep(1)
         rospy.logwarn("check speak_msg")
-        self.assertTrue(self.speak_msg.command == SoundRequest.PLAY_ONCE)
-        if hasattr(SoundRequest, 'volume'): # volume is added from 0.3.0 https://github.com/ros-drivers/audio_common/commit/da9623414f381642e52f59701c09928c72a54be7#diff-fe2d85580f1ccfed4e23a608df44a7f7
-            self.assertTrue(self.speak_msg.volume > 0)
-        self.assertTrue(len(self.speak_msg.arg) > 0)
-        
-        while self.speak_msg_jp == None:
+        rospy.logwarn(self.speak_msg)
+        while self.speak_msg_jp == []:
             rospy.logwarn("waiting for speak_msg_jp")
             rospy.sleep(1)
-        rospy.logwarn("check speak_msg")
-        self.assertTrue(self.speak_msg_jp.command == SoundRequest.PLAY_ONCE)
-        if hasattr(SoundRequest, 'volume'):
-            self.assertTrue(self.speak_msg_jp.volume > 0)
-        self.assertTrue(len(self.speak_msg_jp.arg) > 0)
+        rospy.logwarn("check speak_msg_jp")
+        rospy.logwarn(self.speak_msg_jp)
+        rospy.logwarn("test speak_msg + speak_msg_jp")
+        for msg in self.speak_msg + self.speak_msg_jp:
+            rospy.logwarn(msg)
+            self.assertTrue(msg.command == SoundRequest.PLAY_ONCE)
+            if hasattr(SoundRequest, 'volume'): # volume is added from 0.3.0 https://github.com/ros-drivers/audio_common/commit/da9623414f381642e52f59701c09928c72a54be7#diff-fe2d85580f1ccfed4e23a608df44a7f7
+                self.assertTrue(msg.volume > 0)
+                self.assertTrue((msg.sound == SoundRequest.SAY and len(msg.arg) > 0) or
+                                (msg.sound == SoundRequest.PLAY_FILE and len(msg.arg) > 0) or
+                                (msg.sound > 0))
 
 if __name__ == '__main__':
     import rostest
